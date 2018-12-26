01bcf17f08652e0ff6e584223ff3c650ca5ef476
Jayesh Wani <jayeshwani25@gmail.com>
2018-12-26 23:05:12 +0530

    My Project Setup successfully

---
+++ ZoujajBackend/src/main/java/com/zoujaj/ZoujajBackend/App.java
@@ -0,0 +1,13 @@
+package com.zoujaj.ZoujajBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
